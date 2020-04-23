8ae98c536165705b9410a5dffbaba9765cc61cff
samme <samme@users.noreply.github.com>
2020-04-23 13:51:16 -0700

    It works

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import update from './update.js';
+import Phaser from 'phaser';
 
 // even though Rollup is bundling all your files together, errors and
 // logs will still point to your original source modules
 console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->');
 
 update();
+
+new Phaser.Game({
+  scene: {
+    create: function () {
+      this.add.text(0, 0, 'Hello', { font: '144px cursive' });
+    }
+  }
+});
