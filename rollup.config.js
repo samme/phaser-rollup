96678427400dcea751985a0fca1ab2d82910c262
samme <samme@users.noreply.github.com>
2020-04-23 15:35:26 -0700

    plugin-replace

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import replace from '@rollup/plugin-replace';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
@@ -14,6 +15,14 @@ export default {
 		sourcemap: true
 	},
 	plugins: [
+		replace({
+			'typeof CANVAS_RENDERER': JSON.stringify(true),
+			'typeof WEBGL_RENDERER': JSON.stringify(true),
+			'typeof EXPERIMENTAL': JSON.stringify(true),
+			'typeof PLUGIN_CAMERA3D': JSON.stringify(false),
+			'typeof PLUGIN_FBINSTANT': JSON.stringify(false),
+			'typeof FEATURE_SOUND': JSON.stringify(true)
+		}),
 		resolve(), // tells Rollup how to find date-fns in node_modules
 		commonjs(), // converts date-fns to ES modules
 		production && terser() // minify, but only in production
